87e533676b84a84fd4873716feb52f423a02e5b4
jayprakash Kumar <jptu143@gmail.com>
2019-03-14 05:31:35 +0530

    Updates

---
+++ precache-manifest.30d96be5fc40aba65842eb4e1c3d299a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "894b45e4763b2afef229",
+    "url": "/phonedir/static/js/runtime~main.95888581.js"
+  },
+  {
+    "revision": "569d9126ee6c40e1dd80",
+    "url": "/phonedir/static/js/main.7e7a79d9.chunk.js"
+  },
+  {
+    "revision": "b43ba2d76bdc02655c76",
+    "url": "/phonedir/static/js/2.67b98335.chunk.js"
+  },
+  {
+    "revision": "569d9126ee6c40e1dd80",
+    "url": "/phonedir/static/css/main.ec763b7a.chunk.css"
+  },
+  {
+    "revision": "fef1322252ffef2b89cad3b051fb0d30",
+    "url": "/phonedir/index.html"
+  }
+];
